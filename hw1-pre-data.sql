95c97f33ad4eafc31bdae6311c7a06f1e1897aaa
Yicong-Huang <hyc541978023@gmail.com>
2019-04-13 13:37:23 -0700

    remove redundent files

--- hw1-pre-data.sql
+++
@@ -1,4 +0,0 @@
-TRUNCATE valid_strings;
-INSERT INTO valid_strings VALUES(1, 'Lorem ipsum dapibus purus a suscipit elit risus ac cras orci ad.', 12);
-INSERT INTO valid_strings VALUES(2, 'Etiam vulputate lobortis ultricies gravida fusce.', 6);
-INSERT INTO valid_strings VALUES(3, 'Taciti vulputate magna ornare macenas dui, donec morbi suspendisse turpis urna, turpis in tempor por.', 15);
